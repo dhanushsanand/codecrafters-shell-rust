b094fe430008c47f97ebd3349874843be641780f
Dhanush Shivanand <dhanushsanand@gmail.com>
2026-05-15 10:23:06 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+use bytes::buf;
+
 fn main() {
     //TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
-    let command = io::stdin().read_line(&mut command ).unwrap();
-    println!("{{}}: command not found", command);
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+    println!("{}: command not found", command.trim());
 }
